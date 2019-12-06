533c8a5988beb68e82c6c88560b2564f61bdeae7
Alex Teodorescu <alexteo10@gmail.com>
2019-12-06 16:53:37 -0500

    Models commented

--- EnterpriseFinalProject/Models/Sponsor.cs
+++ EnterpriseFinalProject/Models/Sponsor.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+// Written By Alex Teodorescu
+// Holds the data to represent a Sponsor
+
 namespace EnterpriseFinalProject.Models
 {
     public class Sponsor
     {
+        // Secondary Key for the team being sponsor
         public int Team { get; set; }
+        // Primary key, unique name
         public String Name { get; set; }
     }
 }
--- EnterpriseFinalProject/Models/Team.cs
+++ EnterpriseFinalProject/Models/Team.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+// Written by Alex Teodorescu
+// Stores information to represent a hockey team
+
 namespace EnterpriseFinalProject.Models
 {
     public class Team
     {
+        // Unique ID
         public int ID { get; set; }
+        // Team Name
         public string Name { get; set; }
+        // Manager's Name
         public string Manager { get; set; }
+        // Coach's Name
         public string Coach { get; set; }
     }
 }
